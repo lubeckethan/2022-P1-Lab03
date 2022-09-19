cccecd76bbc61ca0b81477f559af45d71ecb387a
11707 <ethan.lubeck@vtsd.com>
2022-09-19 10:03:22 -0400

    fef

--- Lab03vst.java
+++ Lab03vst.java
@@ -7,8 +7,35 @@
 public class Lab03vst
 {
 	public static void main(String[] args)
+
 	{
-      System.out.println("Lab03, 80 Point Version\n");
+		System.out.println("Lab03, 100 Point Version\n");
+		int sms = 10000123;
+		System.out.println("Starting milli-seconds:" +sms);
+
+		int h = sms / 3600000;
+		System.out.println("Hours:"+ h);
+
+		int s = sms % 3600000;
+		int m = s / 60000;
+		System.out.println("Minutes:" +m);
+
+		int sr = s % 60000;
+		int ms = sr / 1000;
+		System.out.println("Seconds:" +ms);
+
+		int rms = sr % 1000;
+		System.out.println("Milli Seconds:"+rms);
+
+
+
+
+
+
+
+
+
+
       
 	}
 }
