b41f97f02880134f4ec44ed8662ced170d35d0a1
11707 <ethan.lubeck@vtsd.com>
2022-09-30 08:56:07 -0400

    100pt new

--- Lab03vst.java
+++ Lab03vst.java
@@ -11,21 +11,21 @@ public class Lab03vst
 	{
 		System.out.println("Lab03, 100 Point Version\n");
 		int sms = 10000123;
-		System.out.println("Starting milli-seconds:" +sms);
+		System.out.println("Starting milli-seconds:" +"		    " +sms);
 
 		int h = sms / 3600000;
-		System.out.println("Hours:"+ h);
+		System.out.println("Hours:"+" 						     " +h);
 
 		int s = sms % 3600000;
 		int m = s / 60000;
-		System.out.println("Minutes:" +m);
+		System.out.println("Minutes:" + "						"  +m);
 
 		int sr = s % 60000;
 		int ms = sr / 1000;
-		System.out.println("Seconds:" +ms);
+		System.out.println("Seconds:"+" 				    	" +     ms);
 
 		int rms = sr % 1000;
-		System.out.println("Milli Seconds:"+rms);
+		System.out.println("Milli Seconds:"+"				    " +    rms);
 
 
 
---
+++ MathTest.java
@@ -0,0 +1,18 @@
+public class MathTest {
+    public static void main(String args[]) {
+        int n1 = 24;
+        int n2 = 7;
+        double n3 = 15;
+        double n4 = 8;
+
+        double sqrtN1 = Math.sqrt(n1);
+        System.out.println(Math.ceil(sqrtN1));
+
+
+    }
+}
+
+
+
+
+
